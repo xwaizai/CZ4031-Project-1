bd4d78585f45dad745c20dba343b37fed995cd31
xw <pxingw@gmail.com>
2023-01-31 02:45:27 -0500

    testing

--- build/CMakeFiles/executable.dir/DependInfo.cmake
+++ build/CMakeFiles/executable.dir/DependInfo.cmake
@@ -1,17 +1,14 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/home/xingwei/Documents/GitHub/CZ4031 Project 1/src/main.c" "/home/xingwei/Documents/GitHub/CZ4031 Project 1/build/CMakeFiles/executable.dir/src/main.c.o"
-  )
-set(CMAKE_C_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
-  "../include"
-  "../src"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kali/Documents/GitHub/CZ4031-Project-1/src/main.c" "CMakeFiles/executable.dir/src/main.c.o" "gcc" "CMakeFiles/executable.dir/src/main.c.o.d"
   )
 
 # Targets to which this target links.
