744ee3ef2f7e49ff4674e028c59901e2f3a9fd87
xwaizai <pxingw@gmail.com>
2023-01-31 10:38:07 +0800

    initial

---
+++ build/CMakeFiles/executable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/executable.dir/src/main.c.o"
+  "executable"
+  "executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
