dff59846e39e1c2bbfdf9845ea5ebe0839d1787d
xwaizai <pxingw@gmail.com>
2023-02-05 15:03:46 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -56,12 +56,15 @@ memBlock* readFromFile(memBlock* current){
 
                 token = strtok(NULL, d);
             }
-            /*shifting the array*/
-            for(int i = 20;i>11;i--){
-                tobeinserted[i]=tobeinserted[i-1];
+            /*check if rating is below 10*/
+            if(tobeinserted[10] == '.'){
+                /*shifting the array*/
+                for(int i = 20;i>11;i--){
+                    tobeinserted[i]=tobeinserted[i-1];
+                }
+                /*setting header for number of digits of numVotes*/
+                tobeinserted[12]='0';
             }
-            /*setting header for number of digits of numVotes*/
-            tobeinserted[12]=tokenlen;
             
             /*Insert to memory block*/
             current = fillmemBlock(tobeinserted, sizeof(tobeinserted),current);
@@ -80,9 +83,69 @@ memBlock* readFromFile(memBlock* current){
 /*Solely for debuging purposes*/
 void printRecord(char * record){
     for(int i = 0 ; i < 20; i++){
-        printf("%c",*(record+i));
+        printf("%c ",*(record+i));
     }
     printf("\n");
+}
+
+// function to swap elements
+void swap(unsigned int *a, unsigned int *b) {
+  unsigned int t = *a;
+  *a = *b;
+  *b = t;
+}
+void swapAddr(char** a, char** b){
+    char* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// function to find the partition position
+unsigned int partition(unsigned int array[], char** blockaddr,unsigned int low, unsigned int high) {
+  
+  // select the rightmost element as pivot
+  unsigned int pivot = array[high];
+  
+  // pointer for greater element
+  unsigned int i = (low - 1);
+
+  // traverse each element of the array
+  // compare them with the pivot
+  for (unsigned int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+        
+      // if element smaller than pivot is found
+      // swap it with the greater element pointed by i
+      i++;
+      
+      // swap element at i with element at j
+      swap(&array[i], &array[j]);
+      swapAddr(&blockaddr[i],&blockaddr[j]);
+    }
+  }
+
+  // swap the pivot element with the greater element at i
+  swap(&array[i + 1], &array[high]);
+  swapAddr(&blockaddr[i + 1], &blockaddr[high]);
+  
+  // return the partition point
+  return (i + 1);
+}
+
+void quickSort(unsigned int array[],char** blockaddr,unsigned int low, unsigned int high) {
+  if (low < high) {
+    
+    // find the pivot element such that
+    // elements smaller than pivot are on left of pivot
+    // elements greater than pivot are on right of pivot
+    unsigned int pi = partition(array,blockaddr, low, high);
+    
+    // recursive call on the left of pivot
+    quickSort(array,blockaddr, low, pi - 1);
+    
+    // recursive call on the right of pivot
+    quickSort(array,blockaddr, pi + 1, high);
+  }
 }
 
 int main()
@@ -127,7 +190,7 @@ int main()
 
     unsigned int noOfRec = 10*(blocks-1)+numRec;
 
-    unsigned int numVotes[noOfRec];
+    unsigned int *numVotes = (int *) malloc(noOfRec*sizeof(int *));
 
     unsigned int blockaddrIndex = 0;
     char **blockaddr = (char**)malloc(noOfRec*sizeof(char**));
@@ -152,6 +215,14 @@ int main()
             
         }
         current = current->next;
+    }
+    printf("%d\n",noOfRec);
+
+    quickSort(numVotes,blockaddr,0,noOfRec-1);
+
+    for(int i = 0;i<1000;i++){
+        printf("%d ",numVotes[i]);
+        printRecord(blockaddr[i]);
     }
     
     return 0;
